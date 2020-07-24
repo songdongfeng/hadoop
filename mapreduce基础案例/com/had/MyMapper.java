233d3c6098b3c00fe23fab2c884f4c4ee374f95e
songdongfeng <53966928+songdongfeng@users.noreply.github.com>
2020-07-24 12:49:30 +0800

    Update MyMapper.java

--- mapreduce基础案例/com/had/MyMapper.java
+++ mapreduce基础案例/com/had/MyMapper.java
@@ -1,1 +1,25 @@
+package com.had;
 
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class MyMapper extends Mapper<LongWritable,Text,Text,LongWritable> {
+
+    private LongWritable one = new LongWritable(1);
+
+    public void map(LongWritable key, Text value, Context context
+    ) throws IOException, InterruptedException {
+        // value -> "hello world java python"
+        String[] split = value.toString().split(" ");
+        // split -> ["hello","world","java","python"]
+
+        for (String s:split){
+            // set key and value
+            context.write(new Text(s),one);
+        }
+    }
+
+}
